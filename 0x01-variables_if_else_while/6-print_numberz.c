32a24f2220bff9b83200bf62a8a784e90e3f566f
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-07-02 02:45:39 +0000

    update in task 6-Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all the single digit sof base 10
+ * starting from 0 when executed
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
