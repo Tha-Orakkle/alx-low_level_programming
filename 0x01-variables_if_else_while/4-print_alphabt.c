1d01dbf6d68a0eb0667a8321d6fbbfe60d616574
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-07-02 02:30:25 +0000

    update on task 4-When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet except
+ * letters 'q' and 'e' when executed
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != e && ch != q)
+		{
+			putchar(ch)
+		}
+	}
+	putchar('\n');
+	return (0);
+}
