a26f093356dab9f884b661af9c8d1693a3952dc8
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-07-05 23:18:43 +0000

    update on task 2-10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 tines
+ * in lowercase and is followed by a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch = 'a';
+	int i = 0;
+
+	while (i < 10)
+	{
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_puchar('\n');
+		i++;
+	}
+}
