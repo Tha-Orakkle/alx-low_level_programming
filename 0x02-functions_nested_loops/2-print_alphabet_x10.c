edeca4a017edaf6383332c24cc317f909f8f0637
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-07-05 23:41:53 +0000

    update on task 2-10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,26 +4,21 @@
  * in lowercase and is follwed by a new line
  */
 
-int main(void)
+void print_alphabet_x10(void)
 {
-	void print_alphabet_x10(void)
+	char ch;
+	int i;
+
+	i = 0;
+	while (i < 10)
 	{
-		char ch;
-		int i;
-
-		i = 0;
-
-		while (i < 10)
+		ch = 'a';
+		while (ch <= 'z')
 		{
-			ch = 'a';
-			while (ch <= 'z')
-			{
-				_putchar(ch);
-				ch++;
-			}
-			_putchar('\n');
-			i++;
+			_putchar(ch);
+			ch++;
 		}
+		_putchar('\n');
+		i++;
 	}
-	return (0);
 }
