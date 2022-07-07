5ac8b6549bc7693655eb38cfc8a1b265e205826b
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-07-07 03:00:08 +0000

    update on task 13-Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - computes and prints all th emultiples of
+ * 3 and 5 below 1024
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int sum3, sum5;
+	int i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			sum3 = sum3 + i;
+		} else if ((i % 5) == 0)
+		{
+			sum5 = sum5 + i;
+		}
+	}
+	printf("%d\n", (sum3 + sum5));
+	return (0);
+}
