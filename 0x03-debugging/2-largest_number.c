2f25339740a08cbcaa30c112417f0abfef548790
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-07-07 14:57:36 +0000

    Debugging task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,14 +20,14 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else
+	else if (c > b)
 	{
 		largest = c;
 	}
-	/*else
+	else
 	{
 		largest = b;
-	}*/
+	}
 
 	return (largest);
 }
