282795b783d79ea4f33394c76bc2164e77ab5ca2
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-07-08 10:27:03 +0000

    More functions task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,13 @@ void print_most_numbers(void)
 	int i;
 
 	i = '0';
-	while (i <= '9' && i != '2' && i != '4')
+	while (i <= '9')
 	{
-		while (i != '2' || i != '4')
-			;
-		i++;
+	       if (i != '2' && i != '4')
+	       {
+		       _putchar(i);
+	       }
+	       i++;
 	}
 	_putchar('\n');
 }
