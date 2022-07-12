8dbf7b9a93adca52a9e0d776390b2d9365bb627f
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-07-12 16:12:34 +0000

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the value of an object to 98
+ * @n: a pointer that points to the address of the object
+ */
+
+void reset_to_98(int *n)
+{
+	int *ptr = n;
+
+	*ptr = 98;
+}
