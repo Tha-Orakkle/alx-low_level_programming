d55b90924a3b0da1d2b657b7c3589a0718d2436c
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-07-13 00:56:59 +0000

    prints every other character of a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	int i, j, len;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	len = i - 1;
+
+	j = len;
+	while (j >= 0)
+	{
+		j++;
+	}
+}
