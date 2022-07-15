d81c3bdbbbb9784c8bbea0638e464a9613a502c2
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-07-15 15:19:33 +0000

    rot13 - Ceasar cypher

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes with the ceasar cypher. It replaces a
+ * letter with the 13th letter after it.
+ * @s: string
+ * Return: resulting string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char *str1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabecdeghijklmnopqrstuvwxyz";
+	char *str2 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	/* checks through s*/
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; str1[j] != '\0'; j++)
+		{
+			if (s[i] == str1[j])
+			{
+				s[i] = str2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ void reverse_array(int *a, int n);
 void reverse_array(int *a, int n);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
 
 #endif
