3f27d8354c22f736b78e75dc4b843dafffa1394f
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-07-24 10:13:16 +0000

    object files and other files

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: is the letter to check for
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+}
