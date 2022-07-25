1733f6ba2116d9e5be21ac187f0ad6f44727c02e
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-07-25 21:04:49 +0000

    Creates newly allocated space in the memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strdup - returns the pointer to a newly allocated memory
+ * @str: string passed
+ * Return: NULL if str is 0 and if insufficient memory is available.
+ */
+
+char *_strdup(char *str)
+{
+	int i, len;
+	char *new_str;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	/* finds the length of the string passed */
+
+	for (len = 0; len <= '\0'; len++)
+		;
+
+	/* allocates new memory space for a string*/
+	new_str = malloc(sizeof(char) * len);
+
+	/* Create a copy of the string in the new space*/
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		new_str[i] = str[i];
+	}
+	return (new_str);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,3 +1,5 @@
 # PROJECT ON MALLOC AND FREE
 ## 0-create_array.c
 * Creates an array of characters.
+## 1-strdup.c
+* Creates a newly allocated space in the memory
