951c700192d440eaff0c7cba8dc46abb063cc0c9
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-07-26 03:04:47 +0000

    create a new memory location for a concatenated string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,81 @@
+#include "main.h"
+
+char *_strcat(char *str1, char *str2);
+int length(char *s);
+
+/**
+ * str_concat - creates a new allocated space in the memory for
+ * a concatenated string
+ * @s1: string one
+ * @s2: string two
+ *
+ * Return: NULL or pointer to new allocate memory
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *new_str;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	new_str = (char *) malloc(sizeof(char) * (length(s1) + length(s2)) + 1);
+	if (new_str == NULL)
+	{
+		return (NULL);
+	}
+
+	else
+	{
+
+		_strcat(new_str, s1);
+		_strcat(new_str, s2);
+	}
+
+	return (new_str);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * length - finds the length of a string
+ * @s: string passed
+ *
+ * Return: length
+ */
+int length(char *s)
+{
+	int n;
+
+	for (n = 0; s[n] != '\0'; n++)
+		;
+	return (n);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -2,4 +2,6 @@
 ## 0-create_array.c
 * Creates an array of characters.
 ## 1-strdup.c
-* Creates a newly allocated space in the memory
+* Creates a newly allocated space in the memory for a string and duplicate the string in the new memory.
+## 2-str_concat.c
+* Creates a newly allocated space in memory for a concatenated string.
