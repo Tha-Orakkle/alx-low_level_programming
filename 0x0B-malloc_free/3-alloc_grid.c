8c2d6ab93ec7b0d0aced1e2da7c97ec0cb9e327d
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-07-27 04:07:33 +0000

    creates and frees memory space for grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,19 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr = (int **) malloc(sizeof(int) * (width * height));
+	arr = (int **) malloc(sizeof(int *) * height);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = (int *) malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			return (NULL);
+		}
+	}
 
 	for (i = 0; i < height; i++)
 	{
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -14,7 +14,7 @@ void free_grid(int **grid, int height)
 	{
 		return;
 	}
-	
+
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
