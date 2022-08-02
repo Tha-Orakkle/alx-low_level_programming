e3808c01715207706323133b3567b765d45bef15
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-08-02 09:48:04 +0000

    an header file for struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - contains data of dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+
+#endif
