0291ded191813c8826fa04127f833cc76fda4bd3
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-08-12 09:26:08 +0000

    assembly language that prints Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,33 +1,9 @@
-	.arch armv8-a
-	.file	"101-hello_holberton.c"
-	.text
-	.section	.rodata
-	.align	3
-.LC0:
-	.string	"Hello, Holberton"
-	.text
-	.align	2
-	.global	main
-	.type	main, %function
+	 global main
+	 extern printf
 main:
-.LFB0:
-	.cfi_startproc
-	stp	x29, x30, [sp, -16]!
-	.cfi_def_cfa_offset 16
-	.cfi_offset 29, -16
-	.cfi_offset 30, -8
-	mov	x29, sp
-	adrp	x0, .LC0
-	add	x0, x0, :lo12:.LC0
-	bl	puts
-	mov	w0, 0
-	ldp	x29, x30, [sp], 16
-	.cfi_restore 30
-	.cfi_restore 29
-	.cfi_def_cfa_offset 0
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 11.2.0-19ubuntu1) 11.2.0"
-	.section	.note.GNU-stack,"",@progbits
+	 move edi, format
+	 xor   eax, eax
+	 call  printf
+	 mov 	eax, 0
+	 ret
+format: db `Hello, Holberton\n`,0
