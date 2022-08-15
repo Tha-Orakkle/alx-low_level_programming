accc87d92fda56cfc729043451399b7e4b1ba433
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-08-15 16:54:12 -0700

    deletes head node and returns the value of data(n) of head

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a listint_t
+ * and returns the head nodes data(n)
+ * @head: double pointer to the listint_t
+ * Return: the data(n) of the head nodes or 0 if listint_t list is empty
+ */
+
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *temp;
+
+	if (head == NULL || (*head) != NULL)
+		return (0);
+
+	n = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	(*head) = temp;
+
+	return (n);
+}
+
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -4,4 +4,5 @@
 * File 2-add_nodeint.c - adds a new node to the beginning of the listint_t list
 * File 3-add_nodeint_end.c - adds a new node to the end of the listint_t list
 * File 4-free_listint.c - frees a listint_t list
-* File 5-free_listint2.c - frees a listint_t and sets head to NULL
+* File 5-free_listint2.c - frees a listint_t list and sets head to NULL
+* File 6-pop_listint.c - frees listint_t list and returns the value of data(n) of the listint_list
