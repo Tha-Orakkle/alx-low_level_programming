3c07367e53e5e070fbff99e72d4be911c35824b9
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-08-15 16:34:59 -0700

    frees a listint list and sets head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list and sets
+ * head to NULL
+ * @head: double pointer to listint_t list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (!head)
+		return;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		(*head) = temp;
+	}
+	(*head) = NULL;
+}
