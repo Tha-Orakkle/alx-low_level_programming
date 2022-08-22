7b60ffc948409182f5b952004312ed617665a295
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-08-23 00:58:41 +0100

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,11 +22,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		while (text_content)
-		{
-			text_content++;
+		while (text_content[count])
 			count++;
-		}
 
 		s = write(fd, text_content, count);
 		if (s != count)
