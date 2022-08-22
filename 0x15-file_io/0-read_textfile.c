bec975b4af5de7724f4f7c4de0411c0e436d1b99
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-08-22 22:34:15 +0100

    reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	while (i < letters)
 	{
 		ch = fgetc(fptr);
-		t = write(STDOUT_FILENO, &ch, 1);
+		write(STDOUT_FILENO, &ch, 1);
 		count++;
 	}
 
