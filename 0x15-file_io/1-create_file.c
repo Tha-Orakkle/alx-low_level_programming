367ce452c86a6ca6aee23bd4d7a428a381f5c805
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-08-23 00:41:01 +0100

    creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file
+ * @text_content: string to be added to the new file
+ *
+ * Return: 1 for success, otherwise -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, s, count = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content)
+			count++;
+
+		s = write(fd, text_content, count);
+		if (s != count)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
+
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -1,2 +1,3 @@
 # FILE I/O
-* File 0-read_textfile.c - is a funtion that reads a text file and prints it to the POSIX standard output
+* File 0-read_textfile.c - is a funtion that reads a text file and prints it to the POSIX standard outputi
+* FIle 1-create_file.c - is a function that creates a file
