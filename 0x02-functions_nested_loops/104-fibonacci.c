8b90b0d2c3c237797876e49948ba6ac005623f7e
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-09-07 12:28:51 +0000

    1st to 98th fibonacci terms

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,9 +9,10 @@
 int main(void)
 {
 	unsigned long i = 0, tmp, a = 1, b = 2;
+	unsigned long a1, a2, b1, b2;
 
 	printf("%lu", a);
-	while (i < 98)
+	while (i < 90)
 	{
 		printf(", %lu", b);
 
@@ -20,6 +21,23 @@ int main(void)
 		a = tmp;
 		i++;
 	}
+
+	a1 = a / 1000000000;
+	a2 = a % 1000000000;
+	b1 = b / 1000000000;
+	b2 = b / 1000000000;
+
+	for (i = 90; i < 98; i++)
+	{
+		printf(", %lu", b1 + (b2 / 1000000000));
+		printf("%lu", b2 % 1000000000);
+		b1 = b1 + a1;
+		a1 = b1 - a1;
+		b2 = b2 + a2;
+		a2 = b2 - a2;
+	}
+
+
 	printf("\n");
 
 	return (0);
