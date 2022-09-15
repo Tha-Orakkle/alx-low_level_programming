aebe744258c3e64ad6842e9858eb9ec00221094a
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-09-15 18:13:59 +0100

    updating the reverse array of integers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,13 +9,11 @@ void reverse_array(int *a, int n)
 {
 	int i, tmp;
 
-	n = n - 1;
-
 	for (i = 0; i < n / 2; i++)
 	{
 		tmp = a[i];
-		a[i] = a[n];
-		a[n] = tmp;
+		a[i] = a[n - 1];
+		a[n - 1] = tmp;
 		n--;
 	}
 }
