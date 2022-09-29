3459e08f2deab4d40e59be9d83836ba4af621eca
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-09-29 15:00:16 +0000

    node at an index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,7 +11,7 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *current = head;
-	int i = 0, count;
+	int i = 0, count = 0;
 
 	if (!head)
 		return (NULL);
