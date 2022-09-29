d10924f6796a9829731fb6b2304f33437e3fed00
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-09-29 13:47:57 +0000

    adds new node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,11 +11,11 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *new;
 
 	if (!head)
 		return (NULL);
-
+	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
 	new->n = n;
