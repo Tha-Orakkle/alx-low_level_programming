234b74f5ebaa80660d41d6c23f38e73debcbff73
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-09-29 14:55:50 +0000

    node at an index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * *get_dnodeint_at_index - gets node at specific index
+ * of a doubly linked list
+ * @head: linked list
+ * @index: index of list
+ *
+ * Return: nth node, otherwise NULL
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current = head;
+	int i = 0, count;
+
+	if (!head)
+		return (NULL);
+	while (current)
+	{
+		count++;
+		current = current->next;
+	}
+	if (index >= count)
+		return (NULL);
+	while (i <= index)
+		head = head->next;
+
+	return (head);
+}
+
