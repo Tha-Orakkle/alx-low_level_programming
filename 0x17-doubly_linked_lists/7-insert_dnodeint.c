811fca13a96012e3ba1a376b0ac1eaba1d644971
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-09-29 17:59:48 +0000

    inserts new node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -42,6 +42,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		backward = current;
 		current = current->next;
 		forward = current->next;
+		i++;
 	}
 	new->next = forward;
 	new->prev = backward;
