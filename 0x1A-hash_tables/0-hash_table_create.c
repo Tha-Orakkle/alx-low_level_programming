215a1294f07923b4e88f3238fdabf8f22290d114
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-10-23 23:08:55 +0100

    test 1.2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
 
-	if (size == 0)
+	if (size <= 0)
 		return (NULL);
 
 	table = malloc(sizeof(hash_table_t));
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table->size = size;
 
-	table->array = calloc(table->size, sizeof(hash_node_t));
+	table->array = calloc((size_t)table->size, sizeof(hash_node_t));
 
 	if (!(table->array))
 	{
