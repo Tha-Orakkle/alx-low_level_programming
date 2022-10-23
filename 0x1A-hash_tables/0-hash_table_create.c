cdd68df8142cca8b3a5692c0e19f573e893e0472
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-10-23 23:01:33 +0100

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
+	hash_table_t *tablei = NULL;
 
 	if (size == 0)
 		return (NULL);
@@ -18,10 +18,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!table)
 		return (NULL);
 
-	table->size = size;
-	table->array = calloc(table->size, sizeof(hash_node_t));
+	table->array = malloc(sizeof(hash_node_t) * size);
+
 	if (!(table->array))
 		return (NULL);
+	table->size = size;
 
 	return (table);
 }
