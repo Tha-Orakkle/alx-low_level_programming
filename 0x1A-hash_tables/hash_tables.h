cf53ac73763999a5b0f8417590f3203e3f1b10f3
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-10-23 19:31:58 +0000

    creates an hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,8 @@
 #ifndef HASH_TABLES_H_
 #define HASH_TABLES_H
 
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -30,5 +32,8 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+
 
 #endif
