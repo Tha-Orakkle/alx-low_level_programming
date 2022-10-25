03dc4050c0dca6492f18f17b48565f46f45ea5b5
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2022-10-25 09:07:11 +0000

    creates an hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *table;
+	hash_table_t *table = NULL;
 
 	if (size <= 0)
 		return (NULL);
@@ -19,18 +19,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!table)
 		return (NULL);
 
-	table->size = size;
-
 	table->array = malloc(sizeof(hash_node_t) * size);
 
 	if (table->array == NULL)
 	{
 		free(table);
+		table = NULL;
 		return (NULL);
 	}
 
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
+
+	table->size = size;
 
 	return (table);
 }
