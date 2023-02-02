2fb393bf9e603315d670863b51c5f0a7413646b3
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-02-02 17:34:58 +0100

    prevoious work on hash table cleared

---
+++ 0x06-pointers_arrays_strings/check/103-infinite_add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int len1, len2;
+
+	len1 = strlen(n1);
+	len2 = strlen(n2);
+
+	if (len2 > len1)
+	{
+		max_len = len2;
+		min_len = len1;
+	}
+	else
+	{
+		max_len = len1;
+		min_len = len2;
+	}
+
+	for (i = (max_len - 1); i >= 0; i--)
+	{
+		
+
+
+
+
+}
---
+++ 0x06-pointers_arrays_strings/check/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <string.h>
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+#endif
