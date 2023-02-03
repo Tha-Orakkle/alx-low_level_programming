2d59939b277ca666cb423b439710e09794983bf4
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-02-03 18:26:59 +0100

    retreives the value of a key

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -43,6 +43,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 unsigned long int check_key(hash_node_t *linked_list, const char *key);
 hash_node_t *add_node(hash_node_t **head, const char *key, const char *value);
 void replace_node(hash_node_t **head, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 
 
 #endif
