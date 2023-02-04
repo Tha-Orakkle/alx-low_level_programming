e1756de6db4d628fe8fb9917977dfb9690cd1203
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-02-04 10:42:18 +0100

    deletes a hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -45,5 +45,8 @@ hash_node_t *add_node(hash_node_t **head, const char *key, const char *value);
 void replace_node(hash_node_t **head, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+void linked_list_delete(hash_node_t **head);
+
 
 #endif
